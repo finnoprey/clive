52e95fb793f7a74050ee511164c3a5818b970adc
Finn O'Prey <finn@oprey.co>
2022-04-01 20:34:46 +1100

    Fixed problem with evaluating current time

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const { Client, Intents } = require('discord.js')
 require('dotenv').config()
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS ] })
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+let guild
+let channel
 
 function sendClive() {
-  const guild = client.guilds.cache.get('956129489270759454')
-  const channel = guild.channels.cache.get('956135115585622056')
   channel.send('@everyone Daily Clive Time!\n https://www.youtube.com/watch?v=C1UB1-LKX5E')
 }
 
 client.on('ready', () => {
+  guild = client.guilds.cache.get('956129489270759454')
+  channel = guild.channels.cache.get('956135115585622056')
+
   setInterval(() => {
     let date = new Date()
-    if (date.getHours() == 20 && date.getMinutes == 27 && date.getSeconds == 1) {
+    if (date.getHours() == 20 && date.getMinutes() == 36 && date.getSeconds() == 02) {
       sendClive()
     }
   }, 1000)
+})
+
+client.on('messageCreate', (message) => {
+  if (message.content.includes('frozen') && message.content.includes('trick')) {
+    message.channel.send('It\'s control shift eight.')
+  }
 })
 
 client.login(process.env.TOKEN)
